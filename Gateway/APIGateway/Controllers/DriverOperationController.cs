aba4d7705a18cda015d611f7e3c29c6bdf2dc511
seema.kakadiya <seema.kakadiya@DSK-800>
2021-03-05 18:37:28 +0530

    Driver Operation Added

---
+++ Gateway/APIGateway/Controllers/DriverOperationController.cs
@@ -0,0 +1,36 @@
+﻿using APIGateway.Model;
+using APIGateway.Util;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverOperationController : ControllerBase
+    {
+        HttpClientUtil HttpClient = new HttpClientUtil();
+        [HttpGet, Route("DriverOrders")]
+        public async Task<List<Order>> AssignDriver(int driverId)
+        {
+            List<Order> orders = await HttpClient.RunAsyncGetAll<Order>("https://localhost:44305/api/Order/GetOrder");
+            orders = orders.FindAll(x => x.DriverId == driverId);
+            return orders;
+        }
+        [HttpGet, Route("DriverCurrentOrder")]
+        public async Task<List<Order>> CurrentOrder(int driverId)
+        {
+            List<Order> orders = await HttpClient.RunAsyncGetAll<Order>("https://localhost:44305/api/Order/GetOrder");
+            orders = orders.FindAll(x => x.DriverId == driverId && x.IsDeliver == false);
+            return orders;
+        }
+        [HttpGet, Route("DriverDeliveredOrder")]
+        public async Task<List<Order>> DeliveredOrder(int driverId)
+        {
+            List<Order> orders = await HttpClient.RunAsyncGetAll<Order>("https://localhost:44305/api/Order/GetOrder");
+            orders = orders.FindAll(x => x.DriverId == driverId && x.IsDeliver == true);
+            return orders;
+        }
+    }
+}
